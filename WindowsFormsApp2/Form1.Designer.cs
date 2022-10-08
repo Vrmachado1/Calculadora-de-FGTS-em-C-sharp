24d4ae59b44a9074a2a07d4f2b557d7120272b44
vrmac <vrmac@NOTEDOVINI>
2022-10-08 00:13:05 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInss = new System.Windows.Forms.TextBox();
+            this.txtIrrf = new System.Windows.Forms.TextBox();
+            this.txtSL = new System.Windows.Forms.TextBox();
+            this.txtFgts = new System.Windows.Forms.TextBox();
+            this.txtSB = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInss
+            // 
+            this.txtInss.Location = new System.Drawing.Point(145, 102);
+            this.txtInss.Name = "txtInss";
+            this.txtInss.Size = new System.Drawing.Size(100, 20);
+            this.txtInss.TabIndex = 0;
+            // 
+            // txtIrrf
+            // 
+            this.txtIrrf.Location = new System.Drawing.Point(145, 135);
+            this.txtIrrf.Name = "txtIrrf";
+            this.txtIrrf.Size = new System.Drawing.Size(100, 20);
+            this.txtIrrf.TabIndex = 1;
+            // 
+            // txtSL
+            // 
+            this.txtSL.Location = new System.Drawing.Point(145, 165);
+            this.txtSL.Name = "txtSL";
+            this.txtSL.Size = new System.Drawing.Size(100, 20);
+            this.txtSL.TabIndex = 2;
+            // 
+            // txtFgts
+            // 
+            this.txtFgts.Location = new System.Drawing.Point(145, 73);
+            this.txtFgts.Name = "txtFgts";
+            this.txtFgts.Size = new System.Drawing.Size(100, 20);
+            this.txtFgts.TabIndex = 3;
+            // 
+            // txtSB
+            // 
+            this.txtSB.Location = new System.Drawing.Point(145, 12);
+            this.txtSB.Name = "txtSB";
+            this.txtSB.Size = new System.Drawing.Size(100, 20);
+            this.txtSB.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnCalcular.Location = new System.Drawing.Point(12, 38);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(233, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Salario bruto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(22, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 14);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "FGTS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(22, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "INSS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(22, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "IRRF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(12, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Salario liquido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 215);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSB);
+            this.Controls.Add(this.txtFgts);
+            this.Controls.Add(this.txtSL);
+            this.Controls.Add(this.txtIrrf);
+            this.Controls.Add(this.txtInss);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInss;
+        private System.Windows.Forms.TextBox txtIrrf;
+        private System.Windows.Forms.TextBox txtSL;
+        private System.Windows.Forms.TextBox txtFgts;
+        private System.Windows.Forms.TextBox txtSB;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double SB, Fgts, pfgts, inss, pinss, irrf, pirrf, SL;
+            SB = double.Parse(txtSB.Text);
+            pfgts = 8;
+            Fgts = SB * (pfgts / 100);
+            txtFgts.Text = Fgts.ToString();
+            if (SB <= 1212)
+            {
+                pinss = 7.5;
+                inss = SB * (pinss / 100);
+                txtInss.Text = inss.ToString();
+            }
+            else if (SB <= 2427.35)
+            {
+                pinss = 9;
+                inss = SB * (pinss / 100);
+                txtInss.Text = inss.ToString();
+            }
+            else if (SB <= 3641.03)
+            {
+                pinss = 12;
+                inss = SB * (pinss / 100);
+                txtInss.Text = inss.ToString();
+            }
+            else if (SB <= 7087.22)
+            {
+                pinss = 14;
+                inss = SB * (pinss / 100);
+                txtInss.Text = inss.ToString();
+            }
+            else
+            {
+                inss = 828.38;
+                txtInss.Text = inss.ToString();
+            }
+            if(SB <= 1903.38)
+            {
+                irrf = 0;
+                txtIrrf.Text = irrf.ToString();
+            }
+        else if(SB <= 2826.65)
+            {
+                pirrf = 7.5;
+                irrf = (SB * (pirrf / 100)) - 142.8;
+                txtIrrf.Text = irrf.ToString();
+            }
+        else if(SB <= 3751.05)
+            {
+                pirrf = 15;
+                irrf = (SB * (pirrf / 100)) - 354.80;
+                txtIrrf.Text = irrf.ToString();
+            }
+        else if(SB <= 4664.08)
+            {
+                pirrf = 22.5;
+                irrf = (SB * (pirrf / 100)) - 636.13;
+                txtIrrf.Text = irrf.ToString();
+            }
+            else
+            {
+                pirrf = 27.5;
+                irrf = (SB * (pirrf / 100)) - 869.36;
+                txtIrrf.Text = irrf.ToString();
+            }
+            SL = SB - inss - irrf;
+            txtSL.Text = SL.ToString();
+        }
+
+
+    }
+}
